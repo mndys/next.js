178c61f136d660e97275b97d28330b71d340f12b
Mandy Schwarze <mandy2301@me.com>
2021-10-25 19:04:37 +0200

    ✨ Lesson 3

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus,
+        unde a, quas suscipit vitae facere eveniet magnam enim odit id amet
+        saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus,
+        unde a, quas suscipit vitae facere eveniet magnam enim odit id amet
+        saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
+      </p>
+    </div>
+  );
+};
+
+export default About;
