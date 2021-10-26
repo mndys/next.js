46e4dc74bc3722186295a96e3236602c46569f01
Mandy Schwarze <mandy2301@me.com>
2021-10-26 14:17:57 +0200

    ✨ Lesson 10

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,8 +1,34 @@
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-    </div>
+    <>
+      <head>
+        <title>Ninja List | All Ninjas </title>
+        <meta name="keywords" content="ninjas" />
+      </head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single} href="">
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
