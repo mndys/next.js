849ca8c991b38a2f7718abda675693b1fe9392dc
Mandy Schwarze <mandy2301@me.com>
2021-10-26 13:57:29 +0200

    ✨ Lesson 9

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,24 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus,
-        unde a, quas suscipit vitae facere eveniet magnam enim odit id amet
-        saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus,
-        unde a, quas suscipit vitae facere eveniet magnam enim odit id amet
-        saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
-      </p>
-    </div>
+    <>
+      <head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          natus, unde a, quas suscipit vitae facere eveniet magnam enim odit id
+          amet saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          natus, unde a, quas suscipit vitae facere eveniet magnam enim odit id
+          amet saepe aliquid pariatur! Dolorem sequi magnam pariatur velit!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in est
-        pariatur saepe impedit. Nulla, accusantium delectus! Ut voluptas eveniet
-        esse exercitationem nemo sint doloremque perspiciatis neque. Odit,
-        sapiente maxime!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in est
-        pariatur saepe impedit. Nulla, accusantium delectus! Ut voluptas eveniet
-        esse exercitationem nemo sint doloremque perspiciatis neque. Odit,
-        sapiente maxime!
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in est
+          pariatur saepe impedit. Nulla, accusantium delectus! Ut voluptas
+          eveniet esse exercitationem nemo sint doloremque perspiciatis neque.
+          Odit, sapiente maxime!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in est
+          pariatur saepe impedit. Nulla, accusantium delectus! Ut voluptas
+          eveniet esse exercitationem nemo sint doloremque perspiciatis neque.
+          Odit, sapiente maxime!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
